29ff24d03db1d6230e7df4c06cff0b49ccdf3af2
AkpokigheMarvellous <m.ovigwe@yahoo.com>
2022-09-12 07:39:35 -0700

    Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-
-
 def magic_calculation(a, b):
-    """A function based on a python bytee code"""
-    return (result)
+    res = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                res += (a ** b) / i
+        except:
+            res = b + a
+            break
+    return res

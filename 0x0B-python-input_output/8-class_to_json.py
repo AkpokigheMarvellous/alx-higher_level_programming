3df727dfaaa429a11f0369a60bdcd7bbde12c78a
AkpokigheMarvellous <m.ovigwe@yahoo.com>
2022-09-27 01:39:37 -0700

    Python - Input/Output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Akpokighe O. Marvellous
+
+"""defines a function that returns the dictionary
+    description
+"""
+
+
+def class_to_json(obj):
+    """returns a dictionary decription of a simple data structure"""
+    return obj.__dict__
